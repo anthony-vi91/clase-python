28dbb4b3b3af65046d7ace64ef801d7ee22ac241
avite.es <avite.es@tecnologicoargos.edu.ec>
2025-09-26 00:18:08 +0000

    clase_25092025

--- main.py
+++ main.py
@@ -1,18 +1,65 @@
 nombre = "Juan"
-print("nombre: ", nombre)
+print("Nombre: ", nombre)
 
 #Interpolacion de Strings (mover lineascon alt+flecha arriba abajo)
-print(f"nombre: {nombre}")
+print(f"Nombre: {nombre}")
 #Presentar el tipo de variable
 print(type(nombre))
 
-edad = 25
+edad = 25 #int
 print(f"La edad es: {edad}")
 print(type(edad))
 
 altura = 1.75 #float
-print(f"la altura es: {altura}")
+print(f"La altura es: {altura}")
 print(type(altura))
 
 es_estudiante = True
 print(type(es_estudiante))
+
+#Operadores aritmeticos
+a = 1
+b = 2
+#suma
+resultado = a + b
+print(f"El resultado de suma es: {resultado}")
+#resta
+resultado = a - b
+print(f"El resultado de resta es: {resultado}")
+#multiplicacion
+resultado = a * b
+print(f"El resultado de multiplicacion es: {resultado}")
+#division
+resultado = a / b
+print(f"El resultado de division es: {resultado}")
+#divion entera
+a = 10
+b = 3
+resltado = a // b
+print(f"El resultado de la division entera es: {resultado}")
+
+a = -10
+b = 3
+resultado = a // b #redondea hacia abajo con los negativos
+print(f"El resultado de la division entera es: {resultado}")
+#residuo
+a = 10
+b = 3
+resultado = a % b
+print(f"El resultado del residuo es: {resultado}")
+
+a = 15
+b = 4
+resultado = a % b
+print(f"El resultado del residuo es: {resultado}")
+#potencia
+a = 2
+b = 2
+resultado = a ** b
+print(f"El resultado de la potencia es: {resultado}")
+
+#Operadores relacionales == != >< >= <=
+
+#Operadores logicos and or not
+
+#Asignaciones = += -=
