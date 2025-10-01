dc635b5715fb0472d12fdb3eed574fc2469fbe1d
avite.es <avite.es@tecnologicoargos.edu.ec>
2025-10-01 00:07:41 +0000

    clas_while

---
+++ while-control.py
@@ -0,0 +1,16 @@
+contador = 0
+while contador < 5:
+    print ("Contador: ", contador)
+    contador += 1 # contador = contador + 1
+
+isMayorEdad = True
+edad = 25
+
+while isMayorEdad:
+    if edad < 18:
+        print ("Es mayorde edad: ", edad)
+        isMayorEdad = False
+    edad -= 1
+    print ("Edad: ", edad)
+
+print ("Fin del ciclo")
